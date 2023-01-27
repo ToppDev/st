fec67f700ae4a434ccfd5392dc3387ac3e4d5b92
Thomas Topp <thomas@topp.cc>
2023-01-27 13:52:28 +0100

    Mono font as default

--- config.h
+++ config.h
@@ -9,7 +9,7 @@
 static char *font2[] = {
 	"JoyPixels:pixelsize=11:antialias=true:autohint=true",
 };
-static char *font = "FiraCode Nerd Font:pixelsize=13:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 
 static int borderpx = 2;
 
